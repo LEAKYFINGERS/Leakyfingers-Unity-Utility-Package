ad839ead998588762830fca7df6f102f75790c64
LEAKYFINGERS <weepingdigits@gmail.com>
2020-05-26 11:57:00 +1000

    Added modified standard assets FP controller scripts

---
+++ Leakyfingers Unity Utility Package/Assets/Scripts/DebugGameplayManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LeakyfingersUtility;
+
+public class DebugGameplayManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!ScenePause.Instance.IsScenePaused)
+            {
+                ScenePause.Instance.PauseScene();
+            }
+            else
+            {
+                ScenePause.Instance.UnpauseScene();
+            }
+        }
+    }
+}
