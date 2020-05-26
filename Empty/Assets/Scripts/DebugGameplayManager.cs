a9b1a188776bd0213eebde776231b7a22214b890
LEAKYFINGERS <weepingdigits@gmail.com>
2020-05-26 10:13:16 +1000

    Added Unity project to repo

---
+++ Empty/Assets/Scripts/DebugGameplayManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LeakyfingersUtility;
+
+public class DebugGameplayManager : MonoBehaviour
+{
+    public DialogList TestDialogList;
+    public DialogUIDisplay DialogDisplay;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            DialogDisplay.DisplayDialog(TestDialogList);
+    }
+}
